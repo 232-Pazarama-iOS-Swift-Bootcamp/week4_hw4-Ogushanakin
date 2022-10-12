e49fa1b5f1cd9cb94bcb579c6e3592b41d6c76b2
Ogushanakin <ogzhnakn96@gmail.com>
2022-10-12 15:06:49 +0300

    User Profile UI

---
+++ Flickr App/Flickr App/View/Profile/ProfileCell.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileCell.swift
+//  Flickr App
+//
+//  Created by AKIN on 12.10.2022.
+//
+
+import UIKit
+
+final class ProfileCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .lightGray
+        
+        addSubview(postImageView)
+        postImageView.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
